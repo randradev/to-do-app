b932e29ea809ab3829e401bad964e19b19a3da6f
randradev <randradevald@gmail.com>
2023-12-30 15:30:24 -0300

    captura de eventos en botón y buscador

--- src/components/columnaDerecha/todosearch.jsx
+++ src/components/columnaDerecha/todosearch.jsx
@@ -4,7 +4,15 @@ function TodoSearch() {
     return(
         <div className="search-container">
             <i className="fas fa-search"></i>
-            <input className="to-do-search" placeholder="Busca tus tareas aquí..." />
+            <input className="to-do-search"
+            placeholder="Busca tus tareas aquí..."
+            onChange={(event) => {
+                console.log("Escribiste en el buscador")
+                console.log(event);
+                console.log(event.target);
+                console.log(event.target.value); //Imprime el valor ingresado por el usuario
+            }}
+            />
         </div>
     );
 }
--- src/components/columnaIzquierda/createtodobutton.jsx
+++ src/components/columnaIzquierda/createtodobutton.jsx
@@ -1,7 +1,14 @@
 function CreateTodoButton() {
     return (
         <div className="button-container">
-            <button className="create-to-do-button">
+            <button className="create-to-do-button"
+                    onClick={
+                        (event) => {
+                            console.log("botón cliqueado")
+                            console.log(event)
+                            console.log(event.target)
+                            }
+                    }>
                 Crear Tarea <i className="fa-solid fa-arrow-right"></i>
             </button>
         </div>
