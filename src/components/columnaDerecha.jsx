8ae9cd4383e623f55fe228b272deb18e866400e1
randradev <randradevald@gmail.com>
2023-12-30 16:41:52 -0300

    buscador completado

--- src/components/columnaDerecha.jsx
+++ src/components/columnaDerecha.jsx
@@ -8,18 +8,30 @@ import './columnaDerecha.css';
 
 
 function ColumnaDerecha() {
+    //Tareas por defecto
     const defaultToDos = [
         { text: 'Create React App', completed: true},
         { text: 'git init', completed: false},
         { text: 'git push origin main', completed: true},
         { text: 'build app structure', completed: false}
       ];
-      
+    
+    //Estado del buscador  
     const [toDos, setToDos] = React.useState(defaultToDos);
     const [searchValue, setSearchValue] = React.useState('');
-
+    
+    //Contador
     const completedToDos = toDos.filter( toDo => !!toDo.completed).length;
     const totalToDos = toDos.length;
+
+    //Buscador
+    const searchedToDos = toDos.filter(
+        (toDos) => {
+            const toDoText = toDos.text.toLowerCase();
+            const searchText = searchValue.toLocaleLowerCase();
+            return toDos.text.includes(searchText);
+        }
+    );
 
     return (
         <div className="container-columna-derecha">
@@ -32,7 +44,7 @@ function ColumnaDerecha() {
                 setSearchValue={setSearchValue}
             />
             <TodoList>
-                {defaultToDos.map(todo => (
+                {searchedToDos.map(todo => (
                 <TodoItem
                     key={todo.text}
                     text={todo.text}
